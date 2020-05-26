23afdb931d949f9bf9a4e00aa1ef1efd783ed647
kaajoj <kaajooj@gmail.com>
2020-05-27 01:11:34 +0200

    Git init

---
+++ CryptoControllerTest.cs
@@ -0,0 +1,86 @@
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Moq;
+using VSApi.Controllers;
+using VSApi.Models;
+using Xunit;
+
+namespace VSapi.Tests
+{
+    public class CryptoControllerTest
+    {
+        // Test Get() method
+        [Fact]
+        public async void Get()
+        {
+            #region Arrange
+            var options = new DbContextOptionsBuilder<ApiContext>()
+                .UseInMemoryDatabase(databaseName: "WebApp")
+                .Options;
+
+            var operationalStoreOptions = Options.Create(new OperationalStoreOptions());
+
+            await using (var context = new ApiContext(options, operationalStoreOptions))
+            {
+                context.Add(new Crypto()
+                {
+                    Rank = 2,
+                    Name = "Test",
+                    Symbol = "XYZ",
+                    Price = "1234,99",
+                    Change24h = null,
+                    Change7d = null,
+                    OwnFlag = 0
+                });
+                await context.SaveChangesAsync();
+            }
+
+            IActionResult cryptoExist;
+            OkObjectResult crypto;
+
+            #endregion
+
+            #region Act
+
+            await using (var context = new ApiContext(options, null))
+            {
+                var controller = new CryptoController(context);
+                cryptoExist = controller.Get();
+                crypto = controller.Get() as OkObjectResult; 
+            }
+            #endregion
+
+            #region Assert
+            Assert.NotNull(cryptoExist);
+            Assert.Equal("200", crypto.StatusCode.Value.ToString());
+            #endregion
+        }
+
+
+        // Test  GetCmcApi() method
+        [Fact]
+        public async void GetCmcApi()
+        {
+
+        }
+
+        // Test  Post() method
+        [Fact]
+        public async void Post()
+        {
+
+        }
+
+        // Test  Edit(int? id, int flag) method
+        [Fact]
+        public async void Edit()
+        {
+
+        }
+
+
+    }
+}
