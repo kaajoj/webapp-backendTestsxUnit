423d5d44cb3206c20177d830149cbc9ed6b8943d
kaajoj <kaajooj@gmail.com>
2021-01-07 01:59:50 +0100

    Add CoinMarketCapApiServiceTest

---
+++ CoinMarketCapApiServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using VSApi.Data;
+using VSApi.Services;
+using Xunit;
+
+namespace VSApi.Tests
+{
+    public class CoinMarketCapApiServiceTest : IClassFixture<ContextFixture>
+    {
+        private readonly CoinMarketCapApiService _coinMarketCapApiService;
+
+        public CoinMarketCapApiServiceTest(ContextFixture contextFixture)
+        {
+            var cryptoRepository = new CryptoRepository(contextFixture.ApiContext);
+            _coinMarketCapApiService = new CoinMarketCapApiService(cryptoRepository);
+        }
+
+        [Fact]
+        public void CmcGet_ReturnsData()
+        {
+            #region Act
+            var response = _coinMarketCapApiService.CmcGet();
+            #endregion
+
+            #region Assert
+            Assert.Contains("data", response);
+            #endregion
+        }
+
+        [Fact]
+        public void CmcJsonParse_ReturnsParsedCrypto()
+        {
+            #region Arrange
+            var response = _coinMarketCapApiService.CmcGet();
+            dynamic jsonObj = JObject.Parse(response);
+            #endregion
+
+            #region Act
+            var cryptoTemp = _coinMarketCapApiService.CmcJsonParse(jsonObj, 0);
+            #endregion
+
+            #region Assert
+            Assert.Contains("BTC", cryptoTemp.Symbol);
+            #endregion
+        }
+    }
+}
